29c908547e80b1c352eba7f2d4dd33aab15bc921
tadyg8 <78745124+tadyg8@users.noreply.github.com>
2024-08-23 17:41:31 +0200

    landing page with animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Header from './header.jsx'
+import Desc from './desc.jsx'
+
+
 
 function App() {
   return (
     <>
     
       <Header />
-    
+      <Desc />
+          
     </>
   )
 }
---
+++ src/desc.jsx
@@ -0,0 +1,36 @@
+import './css/desc.css'
+
+export default function Desc() {
+    
+    let words = [
+        "You’re gonna need a bigger array.",
+        "Houston, we have a solution.",
+        "I see dead pixels.",
+        "Keep calm and carry code.",
+        "To infinity and beyond... in code!",
+        "I’m the king of the DOM!",
+        "Here’s looking at you, code!",
+        "This is the code you’re looking for.",
+        "You can’t handle the code!",
+        "It’s alive! It’s alive!",
+        "There’s no place like 127.0.0.1.",
+        "Why so serious? Let’s code!",
+        "I love the smell of fresh code in the morning.",
+        "Say ‘hello’ to my little script!",
+    ];
+
+    return (
+        <div className="desc">
+
+            <h2><span className='colorText'>Front-end</span> <br /> developer <br /> portfolio</h2>
+            
+            <div className="boxes">
+                <div className="box1"></div>
+                <div className="box2"></div>
+                <div className="box3"></div>
+                <h3 className='message'>{words[Math.floor(Math.random() * words.length)]}</h3>
+            </div>
+
+        </div>
+    )
+}
